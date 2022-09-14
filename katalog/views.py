d167f28c2fbdc6263cf251b9803ba42fa09e2f0d
caroline_esther <carolineeupanggabean@gmail.com>
2022-09-14 10:58:57 +0700

    Tugas2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+    'list_barang': data_barang_katalog,
+    'nama': 'Caroline Esther U. P.',
+    'id': '2106751915'
+}
+    return render(request, "katalog.html", context)
