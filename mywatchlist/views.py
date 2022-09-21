818fdd8f238776ab2d6fb83f6003730cacefda2c
caroline_esther <carolineeupanggabean@gmail.com>
2022-09-21 19:54:03 +0700

    Tugas 3

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import render
 from mywatchlist.models import mywatchlist
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_mywatchlist(request):
@@ -11,3 +13,19 @@ def show_mywatchlist(request):
     'id': '2106751915'
 }
     return render(request, "mywatchlist.html", context)
+
+def show_xml(request):
+    data = mywatchlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = mywatchlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = mywatchlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = mywatchlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
