225a5bf5be95faf40b2af73db6a82da8367fe58e
caroline_esther <carolineeupanggabean@gmail.com>
2022-09-21 19:59:35 +0700

    Tugas 3

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -22,6 +22,10 @@ def show_json(request):
     data = mywatchlist.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
+def show_html(request):
+    data = mywatchlist.objects.all()
+    return HttpResponse(serializers.serialize("html", data), content_type="application/html")
+
 def show_json_by_id(request, id):
     data = mywatchlist.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
@@ -29,3 +33,7 @@ def show_json_by_id(request, id):
 def show_xml_by_id(request, id):
     data = mywatchlist.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_html_by_id(request, id):
+    data = mywatchlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("html", data), content_type="application/html")
